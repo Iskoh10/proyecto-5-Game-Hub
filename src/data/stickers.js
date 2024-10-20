573b46e1fac8c71d51918dcc7bfdecf6022bdb4c
Iskoh <iscoesp@gmail.com>
2024-10-20 21:02:54 +0200

    update

--- src/data/stickers.js
+++ src/data/stickers.js
@@ -1,21 +1,21 @@
 export const stickers = [
   {
-    url: 'public/assets/abeja.png',
+    url: '/assets/abeja.png',
     id: 'bee',
     value: 1
   },
   {
-    url: 'public/assets/cherries.png',
+    url: '/assets/cherries.png',
     id: 'cherry',
     value: 2
   },
   {
-    url: 'public/assets/flamingo.png',
+    url: '/assets/flamingo.png',
     id: 'flamingo',
     value: 3
   },
   {
-    url: 'public/assets/nasturtium.png',
+    url: '/assets/nasturtium.png',
     id: 'flower',
     value: 4
   }
